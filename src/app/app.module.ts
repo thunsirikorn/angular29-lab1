6325779f07ed361fe67ad705912a0676a5c0814f
thunsirikorn <thunsirikorn.n@gmail.com>
2021-03-04 15:59:29 +0700

    angular1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home2Component
   ],
   imports: [
     BrowserModule
